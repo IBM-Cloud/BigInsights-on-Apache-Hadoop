e956042071bfb793187428042996bb6b14afc8bf
christopher snow <chsnow123@gmail.com>
2016-08-05 20:17:59 +0100

    quieten R packages install

--- examples/Zeppelin/setup.R
+++ examples/Zeppelin/setup.R
@@ -1,3 +1,3 @@
-install.packages('devtools', lib="~/R/packages", repos='http://cran.us.r-project.org')
-install.packages('knitr',    lib="~/R/packages", repos='http://cran.us.r-project.org')
-install.packages('ggplot2',  lib="~/R/packages", repos='http://cran.us.r-project.org')
+install.packages('devtools', lib="~/R/packages", repos='http://cran.us.r-project.org', quiet = TRUE)
+install.packages('knitr',    lib="~/R/packages", repos='http://cran.us.r-project.org', quiet = TRUE)
+install.packages('ggplot2',  lib="~/R/packages", repos='http://cran.us.r-project.org', quiet = TRUE)
